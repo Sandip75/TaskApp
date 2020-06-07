1f393aebc9e2124580984f11839cd802814f7c3b
Sandip75 <sk1957339@gmail.com>
2020-06-07 21:10:34 +0530

    Better :: Insert document in collection

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,10 @@
 const mongodb = require("mongodb");
 
-//#region Connect to mongo db 
+//#region Connect to mongo db
 const MongoClient = mongodb.MongoClient;
 
 const connectionurl = "mongodb://127.0.0.1:27017";
-const databasename  = "taskmanger";
+const databasename = "taskmanger";
 
 MongoClient.connect(
   connectionurl,
@@ -14,16 +14,26 @@ MongoClient.connect(
       console.log("Error occur in mongodb ", error);
       return;
     }
-    
+
     //Create Db connection
     const db = client.db(databasename);
 
-    //insert data
-    db.collection('user').insertOne({
-        name : "Sandip Kumar",
-        age : 24
+    //#region  insert one data
+    db.collection("user").insertOne(
+      {
+        name: "Sandip Kumar",
+        age: 24,
+      },
+      (error, result) => {
+        if (error) {
+          console.log("Error occure while inserting document");
+          return;
+        }
 
-    });
+        console.log("Document inserted .. ", result.ops);
+      }
+    );
+    //#endregion
   }
 );
 //#endregion
